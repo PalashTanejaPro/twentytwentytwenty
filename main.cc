51996928e2cbe3b00a0df49163fefaa4bc8ee2cd
PalashTanejaPro <tanejapalash@gmail.com>
2018-03-29 08:10:01 +0530

    works fine

---
+++ main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+
+const int beep_no = 50;
+
+void beep(){
+	for(int i=0;i<beep_no;++i){
+		std::cout<<'\a';
+		std::this_thread::sleep_for(
+			std::chrono::seconds(1));
+	}
+}
+int main(int argc, char **argv){
+	if (argc != 2 || argc != 1){
+		std::cout<<"Usage eyeprotecc [debug]";
+		return 1;
+	}
+
+	std::cout<<"Starting timer.. \n";
+	int sleep_time;
+
+	if(argc==2 && !strncmp("debug", argv[1])){	
+		sleep_time = 1;		
+	}
+	sleep_time = 20;
+
+	std::this_thread::sleep_for(
+			std::chrono::minutes(sleep_time));
+	beep();
+}
